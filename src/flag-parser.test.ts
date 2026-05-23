abca78aa8fe3e024f1fea9191055dc0340121e2c
Erdem Bircan <erdem.erdembircan@gmail.com>
2026-05-23 17:07:51 +0300

    [REFACTOR]: remove --no- negation support from FlagParser

--- src/flag-parser.test.ts
+++ src/flag-parser.test.ts
@@ -12,11 +12,6 @@ describe("FlagParser", () => {
     expect(parser.parse(["--verbose"])).toEqual({ verbose: true });
   });
 
-  it("returns false when a boolean flag is negated with --no- prefix", () => {
-    const parser = new FlagParser({ verbose: { default: true } });
-    expect(parser.parse(["--no-verbose"])).toEqual({ verbose: false });
-  });
-
   it("returns all defined flags when none are present in args", () => {
     const parser = new FlagParser({
       verbose: { default: true },
@@ -28,28 +23,15 @@ describe("FlagParser", () => {
   it("returns all defined flags when only some are present in args", () => {
     const parser = new FlagParser({
       verbose: { default: true },
-      interactive: { default: true },
+      interactive: { default: false },
     });
-    expect(parser.parse(["--no-interactive"])).toEqual({
+    expect(parser.parse(["--verbose"])).toEqual({
       verbose: true,
       interactive: false,
     });
   });
 
-  it("handles multiple flags toggled in a single parse call", () => {
-    const parser = new FlagParser({
-      verbose: { default: true },
-      interactive: { default: true },
-      dry: { default: true },
-    });
-    expect(parser.parse(["--no-verbose", "--no-dry"])).toEqual({
-      verbose: false,
-      interactive: true,
-      dry: false,
-    });
-  });
-
-  it("ignores extra flags not defined in the flag definitions", () => {
+  it("throws for flags not defined in the flag definitions", () => {
     const parser = new FlagParser({ verbose: { default: true } });
     expect(() => parser.parse(["--unknown"])).toThrow();
   });
--- src/flag-parser.ts
+++ src/flag-parser.ts
@@ -14,42 +14,20 @@ export class FlagParser {
     const options: Record<string, { type: "boolean"; default: boolean }> = {};
     const definedNames = new Set(Object.keys(this.#flags));
 
-    // Collect explicit negations from --no-<flag> args, then filter them out
-    const negated = new Set<string>();
-    const filteredArgs: string[] = [];
-
-    for (const arg of args) {
-      if (arg.startsWith("--no-")) {
-        const name = arg.slice(5);
-        if (definedNames.has(name)) {
-          negated.add(name);
-        } else {
-          // Pass through so parseArgs can report it as an unknown option
-          filteredArgs.push(arg);
-        }
-      } else {
-        filteredArgs.push(arg);
-      }
-    }
-
     for (const [name, config] of Object.entries(this.#flags)) {
       options[name] = { type: "boolean", default: config.default };
     }
 
     const { values } = parseArgs({
-      args: filteredArgs,
+      args,
       options,
       allowPositionals: false,
     });
 
     const result: ParsedFlags = {};
     for (const name of definedNames) {
-      if (negated.has(name)) {
-        result[name] = false;
-      } else {
-        result[name] =
-          (values[name] as boolean | undefined) ?? this.#flags[name].default;
-      }
+      result[name] =
+        (values[name] as boolean | undefined) ?? this.#flags[name].default;
     }
 
     return result;
