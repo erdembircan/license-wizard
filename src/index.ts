e1ddc56bdba5b3dbf5a582bfc2fa1038eb1f5c37
Erdem Bircan <erdem.erdembircan@gmail.com>
2026-05-23 17:40:51 +0300

    [FEAT]: implement LicenseWizard bootstrap class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-// Entry point for the license-wizard CLI
+import { FlagParser } from "./flag-parser.js";
+
+const flagParser = new FlagParser({
+  verify: { type: "boolean", default: false },
+});
+
+export class LicenseWizard {
+  constructor(args: string[]) {
+    const flags = flagParser.parse(args);
+    console.log("license-wizard started with flags:", flags);
+  }
+}
