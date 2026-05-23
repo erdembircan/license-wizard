ea3942d7c0ac375acd8c3a19e5be8d03afc79f26
Erdem Bircan <8729400+erdembircan@users.noreply.github.com>
2026-05-23 17:27:32 +0300

    [FEAT]: implement FlagParser class (#19)

---
+++ src/flag-parser.test.ts
@@ -0,0 +1,80 @@
+import { describe, it, expect } from "vitest";
+import { FlagParser } from "./flag-parser.js";
+
+describe("FlagParser", () => {
+  describe("boolean flags", () => {
+    it("returns the default value for a flag that is not present in args", () => {
+      const parser = new FlagParser({ verbose: { type: "boolean", default: true } });
+      expect(parser.parse([])).toEqual({ verbose: true });
+    });
+
+    it("returns true for a boolean flag that is present in args", () => {
+      const parser = new FlagParser({ verbose: { type: "boolean", default: true } });
+      expect(parser.parse(["--verbose"])).toEqual({ verbose: true });
+    });
+
+    it("returns false as default for a boolean flag with false default", () => {
+      const parser = new FlagParser({ interactive: { type: "boolean", default: false } });
+      expect(parser.parse([])).toEqual({ interactive: false });
+    });
+  });
+
+  describe("string flags", () => {
+    it("returns the default value for a string flag that is not present in args", () => {
+      const parser = new FlagParser({ output: { type: "string", default: "stdout" } });
+      expect(parser.parse([])).toEqual({ output: "stdout" });
+    });
+
+    it("returns the provided value for a string flag present in args", () => {
+      const parser = new FlagParser({ output: { type: "string", default: "stdout" } });
+      expect(parser.parse(["--output", "file.txt"])).toEqual({ output: "file.txt" });
+    });
+  });
+
+  describe("multiple flags", () => {
+    it("returns all defined flags when none are present in args", () => {
+      const parser = new FlagParser({
+        verbose: { type: "boolean", default: true },
+        interactive: { type: "boolean", default: true },
+      });
+      expect(parser.parse([])).toEqual({ verbose: true, interactive: true });
+    });
+
+    it("returns all defined flags when only some are present in args", () => {
+      const parser = new FlagParser({
+        verbose: { type: "boolean", default: true },
+        interactive: { type: "boolean", default: false },
+      });
+      expect(parser.parse(["--verbose"])).toEqual({
+        verbose: true,
+        interactive: false,
+      });
+    });
+
+    it("handles mixed boolean and string flags", () => {
+      const parser = new FlagParser({
+        verbose: { type: "boolean", default: false },
+        output: { type: "string", default: "stdout" },
+      });
+      expect(parser.parse(["--verbose", "--output", "file.txt"])).toEqual({
+        verbose: true,
+        output: "file.txt",
+      });
+    });
+  });
+
+  it("silently ignores boolean-style unknown flags", () => {
+    const parser = new FlagParser({ verbose: { type: "boolean", default: false } });
+    expect(parser.parse(["--unknown"])).toEqual({ verbose: false });
+  });
+
+  it("silently ignores string-style unknown flags and their values", () => {
+    const parser = new FlagParser({ verbose: { type: "boolean", default: false } });
+    expect(parser.parse(["--unknown", "value"])).toEqual({ verbose: false });
+  });
+
+  it("keeps defined flags when unknown flags are also present", () => {
+    const parser = new FlagParser({ verbose: { type: "boolean", default: false } });
+    expect(parser.parse(["--verbose", "--unknown"])).toEqual({ verbose: true });
+  });
+});
---
+++ src/flag-parser.ts
@@ -0,0 +1,52 @@
+import { parseArgs } from "node:util";
+
+type FlagType = "boolean" | "string";
+
+type FlagValueType<T extends FlagType> = T extends "boolean" ? boolean : string;
+
+type FlagDefinition<T extends FlagType = FlagType> = {
+  type: T;
+  default: FlagValueType<T>;
+};
+
+type FlagDefinitions = Record<string, FlagDefinition>;
+
+type ParsedFlags<T extends FlagDefinitions> = {
+  [K in keyof T]: FlagValueType<T[K]["type"]>;
+};
+
+export class FlagParser<T extends FlagDefinitions> {
+  readonly #flags: T;
+
+  constructor(flags: T) {
+    this.#flags = flags;
+  }
+
+  parse(args: string[]): ParsedFlags<T> {
+    const options: Record<
+      string,
+      { type: "boolean" | "string"; default: boolean | string }
+    > = {};
+    const definedNames = Object.keys(this.#flags);
+
+    for (const [name, config] of Object.entries(this.#flags)) {
+      options[name] = { type: config.type, default: config.default };
+    }
+
+    const { values } = parseArgs({
+      args,
+      options,
+      allowPositionals: true,
+      strict: false,
+    });
+
+    const result = {} as ParsedFlags<T>;
+    for (const name of definedNames) {
+      (result as Record<string, boolean | string>)[name] =
+        (values[name] as boolean | string | undefined) ??
+        this.#flags[name].default;
+    }
+
+    return result;
+  }
+}
