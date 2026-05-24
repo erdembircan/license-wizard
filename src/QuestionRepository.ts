c9f7e341572700cb90ae8173acb30587f09173f2
Erdem Bircan <8729400+erdembircan@users.noreply.github.com>
2026-05-24 21:21:23 +0300

    [FEAT]: implement QuestionRepository and Orchestrator (#32)

---
+++ src/QuestionRepository.ts
@@ -0,0 +1,22 @@
+import type { Question } from "./Question.js";
+
+/**
+ * Stores and provides access to a collection of questions by index.
+ */
+export class QuestionRepository {
+  readonly #questions: Question[];
+
+  /**
+   * Creates a new QuestionRepository with the given questions.
+   */
+  constructor(questions: Question[]) {
+    this.#questions = [...questions];
+  }
+
+  /**
+   * Returns the question at the given index, or null if the index is out of bounds.
+   */
+  getByIndex(index: number): Question | null {
+    return this.#questions[index] ?? null;
+  }
+}
