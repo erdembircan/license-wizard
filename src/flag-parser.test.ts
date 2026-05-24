2d64a2803224f567f23d7592d668cef93a7dd052
Erdem Bircan <erdem.erdembircan@gmail.com>
2026-05-24 14:36:12 +0300

    [STYLE]: fix prettier formatting in build script and tests

--- src/flag-parser.test.ts
+++ src/flag-parser.test.ts
@@ -4,30 +4,42 @@ import { FlagParser } from "./flag-parser.js";
 describe("FlagParser", () => {
   describe("boolean flags", () => {
     it("returns the default value for a flag that is not present in args", () => {
-      const parser = new FlagParser({ verbose: { type: "boolean", default: true } });
+      const parser = new FlagParser({
+        verbose: { type: "boolean", default: true },
+      });
       expect(parser.parse([])).toEqual({ verbose: true });
     });
 
     it("returns true for a boolean flag that is present in args", () => {
-      const parser = new FlagParser({ verbose: { type: "boolean", default: true } });
+      const parser = new FlagParser({
+        verbose: { type: "boolean", default: true },
+      });
       expect(parser.parse(["--verbose"])).toEqual({ verbose: true });
     });
 
     it("returns false as default for a boolean flag with false default", () => {
-      const parser = new FlagParser({ interactive: { type: "boolean", default: false } });
+      const parser = new FlagParser({
+        interactive: { type: "boolean", default: false },
+      });
       expect(parser.parse([])).toEqual({ interactive: false });
     });
   });
 
   describe("string flags", () => {
     it("returns the default value for a string flag that is not present in args", () => {
-      const parser = new FlagParser({ output: { type: "string", default: "stdout" } });
+      const parser = new FlagParser({
+        output: { type: "string", default: "stdout" },
+      });
       expect(parser.parse([])).toEqual({ output: "stdout" });
     });
 
     it("returns the provided value for a string flag present in args", () => {
-      const parser = new FlagParser({ output: { type: "string", default: "stdout" } });
-      expect(parser.parse(["--output", "file.txt"])).toEqual({ output: "file.txt" });
+      const parser = new FlagParser({
+        output: { type: "string", default: "stdout" },
+      });
+      expect(parser.parse(["--output", "file.txt"])).toEqual({
+        output: "file.txt",
+      });
     });
   });
 
@@ -64,17 +76,23 @@ describe("FlagParser", () => {
   });
 
   it("silently ignores boolean-style unknown flags", () => {
-    const parser = new FlagParser({ verbose: { type: "boolean", default: false } });
+    const parser = new FlagParser({
+      verbose: { type: "boolean", default: false },
+    });
     expect(parser.parse(["--unknown"])).toEqual({ verbose: false });
   });
 
   it("silently ignores string-style unknown flags and their values", () => {
-    const parser = new FlagParser({ verbose: { type: "boolean", default: false } });
+    const parser = new FlagParser({
+      verbose: { type: "boolean", default: false },
+    });
     expect(parser.parse(["--unknown", "value"])).toEqual({ verbose: false });
   });
 
   it("keeps defined flags when unknown flags are also present", () => {
-    const parser = new FlagParser({ verbose: { type: "boolean", default: false } });
+    const parser = new FlagParser({
+      verbose: { type: "boolean", default: false },
+    });
     expect(parser.parse(["--verbose", "--unknown"])).toEqual({ verbose: true });
   });
 });
