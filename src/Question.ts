df264092e97a03f8e807ad61fac8f80e8daa3d37
Erdem Bircan <erdem.erdembircan@gmail.com>
2026-05-25 16:39:49 +0300

    [FEAT]: add autocomplete question type with search callback

--- src/Question.ts
+++ src/Question.ts
@@ -1,7 +1,14 @@
-export type QuestionType = "text" | "confirm";
+export type QuestionType = "text" | "confirm" | "autocomplete";
+
+export type AutocompleteOption = {
+  value: string;
+  label: string;
+  hint?: string;
+};
 
 export type Question = {
   id: string;
   text: string;
   type: QuestionType;
+  search?: (query: string) => Promise<AutocompleteOption[]>;
 };
